1ba57e7198f6db90e088e427cbd399b8b112fd61
spaceboundbear <mf.michaelfischer@gmail.com>
2022-05-09 14:23:04 -0500

    data

---
+++ src/data/Puppies.js
@@ -0,0 +1,62 @@
+const Puppies = [
+  {
+    town: 'Traverse Town',
+    puppyData: [
+      {
+        id: 1,
+        location: 'Mystical House',
+        numbers: '1, 2, 3',
+      },
+      {
+        id: 2,
+        location: 'Alleyway',
+        numbers: '4, 5, 6',
+      },
+      {
+        id: 3,
+        location: 'Item Workshop',
+        numbers: '7, 8, 9',
+      },
+      {
+        id: 4,
+        location: 'Secret Waterway',
+        numbers: '10, 11, 12',
+      },
+    ],
+  },
+  {
+    town: 'Wonderland',
+    puppyData: [
+      {
+        id: 1,
+        location: `Queen's Castle`,
+        numbers: '13, 14, 15',
+      },
+      {
+        id: 2,
+        location: 'Lotus Forest',
+        numbers: '16, 17, 18',
+      },
+      {
+        id: 3,
+        location: 'Tea Party Garden',
+        numbers: '19, 20, 21',
+      },
+      {
+        id: 4,
+        location: 'Lotus Forest',
+        numbers: '58, 59, 60',
+      },
+    ],
+  },
+  {
+    town: 'Olympus Coliseum',
+    puppyData: [
+      {
+        id: 1,
+        location: `Castle Gates`,
+        numbers: '22, 23, 24',
+      },
+    ],
+  },
+];
---
+++ src/data/RouteMenus.js
@@ -0,0 +1,36 @@
+const RouteMenus = [
+  {
+    town: 'Traverse Town',
+    menus: [
+      {
+        id: 1,
+        when: 'After You Get Dodge Roll',
+        list: [
+          {
+            option: 'Items',
+            instr: "Give Goofy's Ether to Sora",
+          },
+          {
+            option: 'Equipment',
+            instr: 'Sora: Equip Brave Warrior over Ribbon',
+          },
+        ],
+      },
+      {
+        id: 2,
+        location: 'Alleyway',
+        numbers: '4, 5, 6',
+      },
+      {
+        id: 3,
+        location: 'Item Workshop',
+        numbers: '7, 8, 9',
+      },
+      {
+        id: 4,
+        location: 'Secret Waterway',
+        numbers: '10, 11, 12',
+      },
+    ],
+  },
+];
